b16fb08a6dd1932ac5d4775914fca5e904b2dd8d
Jonathan Saji <jonathansaji2009@gmail.com>
2026-02-28 17:30:53 -0500

    movement added

--- beneath-the-surface/movement.gd
+++ beneath-the-surface/movement.gd
@@ -1,25 +1,48 @@
 extends CharacterBody2D
 
+const SPEED = 600.0
+const JUMP_VELOCITY = -700.0
+const GRAVITY = 980.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+@onready var anim = $AnimatedSprite2D
+ 
+func _physics_process(delta : float):
+	
+	#this applies gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+	#Jump
+	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	
+	#horizontal movement
+	var direction = Input.get_axis("left","right")
+	
+	
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x,0,SPEED)
+		
+	#flipping sprite based on the dir
+	if direction > 0:
+		anim.flip_h = false
+	elif direction < 0:
+		anim.flip_h = true
+		
 	move_and_slide()
+"""
+	if not is_on_floor():
+		if anim.animation != "jump":
+			anim.play("jump")
+	elif direction != 0:
+		if anim.animation != "run":
+			anim.play("run")
+	else:
+		if anim.animation != "idle":
+			anim.play("idle")
+"""
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print(anim)
