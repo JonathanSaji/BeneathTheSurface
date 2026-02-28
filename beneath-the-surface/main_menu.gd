628d18201a21461d258a238fd3916ff8c5dfdac5
Jonathan Saji <jonathansaji2009@gmail.com>
2026-02-28 15:40:35 -0500

    basic menu

---
+++ beneath-the-surface/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var play_btn    : Button = $ColorRect/VBoxContainer/Play
+@onready var options_btn : Button = $ColorRect/VBoxContainer/Options
+@onready var quit_btn    : Button = $ColorRect/VBoxContainer/Quit
+
+
+func _ready() -> void:
+	play_btn.pressed.connect(_on_play_pressed)
+	options_btn.pressed.connect(_on_options_pressed)
+	quit_btn.pressed.connect(_on_quit_pressed)
+
+func _on_play_pressed() -> void:
+	# get_tree().change_scene_to_file("res://Game.tscn")
+	print("Play Pressed")
+
+func _on_options_pressed() -> void:
+	print("Options pressed")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
