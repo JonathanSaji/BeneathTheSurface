074b8a3c628df87961ef0402294ba6979ecfba96
Sebx00 <sc029422@gmail.com>
2026-02-28 16:42:41 -0500

    Switch scenes to the prologue

--- beneath-the-surface/main_menu.gd
+++ beneath-the-surface/main_menu.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 	play_btn.pressed.connect(_on_play_pressed)
 	options_btn.pressed.connect(_on_options_pressed)
 	quit_btn.pressed.connect(_on_quit_pressed)
+	anim.animation_finished.connect(_on_animation_finished)
 
 func _on_play_pressed() -> void:
 	# get_tree().change_scene_to_file("res://Game.tscn")
@@ -20,3 +21,7 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_animation_finished(name):
+	if name == "scroll_up":
+		get_tree().change_scene_to_file("res://prologue.tscn")
